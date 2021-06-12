b23a326355d6f94b1376416597026684946c0037
Sergey <Rsmon1982@yandex.ru>
2021-06-12 15:11:59 +0400

    Add scroll parallax effect

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = function() {
     const parallax = document.querySelector('.parallax');
 
     if(!!parallax) {
-        const content = document.querySelector('.parallax-container');
+        const content = document.querySelector('.parallax__container');
         const clouds = document.querySelector('.images-parallax__clouds');
         const mountains = document.querySelector('.images-parallax__mountains');
         const human = document.querySelector('.images-parallax__human');
@@ -54,6 +54,28 @@ window.onload = function() {
         });
 
         //Parallax при скролле
+
+        let threshholdSets = [];
+        for (let i = 0; i < 1.0; i += 0.05) {
+            threshholdSets.push(i)
+        }
+
+        const callback = function (entries, observer) {
+            const scrollTopPercent = window.pageYOffset / parallax.offsetHeight * 100;
+            setParallaxItemsStyle(scrollTopPercent);
+        }
+
+        const observer = new IntersectionObserver(callback, {
+            threshold: threshholdSets
+        })
+
+        observer.observe(document.querySelector('.content'))
+
+        function setParallaxItemsStyle(scrollTopPercent) {
+            content.style.cssText = `transform: translate(0%, -${scrollTopPercent / 9}%);`;
+            mountains.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 6}%);`;
+            human.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 3}%);`;
+        }
         
     }   
 }
